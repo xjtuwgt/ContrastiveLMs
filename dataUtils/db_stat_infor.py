ea245c957c086074af5cfd863f97e5837be9132c
guangtao.wang <guangtao.wang@jd.com>
2021-02-08 18:13:14 -0800

    Initial commit

--- dataUtils/db_stat_infor.py
+++ dataUtils/db_stat_infor.py
@@ -17,6 +17,8 @@ def hyper_link_ner_extractor(doc_db: DocDB, title_to_id: dict):
         assert len(text_with_links) == len(text_ner)
         sent_number_list.append(len(text_with_links))
         ent_num_sent = [len(_) for _ in text_ner]
+        if 0 in ent_num_sent:
+            print(text_ner)
         ent_para = sum(ent_num_sent)
         ent_num_sent_list += ent_num_sent
         ent_num_para_list.append(ent_para)
