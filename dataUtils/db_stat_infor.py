7d5b3c3bdfb6bd1d10472e76e7a8f8a7fc38f7d8
guangtao.wang <guangtao.wang@jd.com>
2021-02-08 18:26:22 -0800

    Initial commit

--- dataUtils/db_stat_infor.py
+++ dataUtils/db_stat_infor.py
@@ -66,5 +66,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     doc_db = DocDB(db_path=args.db_path)
-    title_to_id = title_to_id_extractor(doc_db=doc_db, row_num=10000)
+    title_to_id = title_to_id_extractor(doc_db=doc_db, row_num=200000)
     hyper_link_ner_extractor(doc_db=doc_db, title_to_id=title_to_id)
